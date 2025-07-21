4212682ae32dcb6e4f52658d6a17a88a8f0352d3
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-21 08:58:48 +0530

    Delete components/helpers directory

--- components/helpers/eco_score.py
+++
@@ -1,9 +0,0 @@
-def calculate(product):
-    score = 100
-    if 'plastic' in product.get('material', '').lower():
-        score -= 30
-    if product.get('origin', '').lower() != 'local':
-        score -= 20
-    if product.get('packaging', '').lower() in ['plastic', 'non-recyclable']:
-        score -= 20
-    return max(score, 0)
