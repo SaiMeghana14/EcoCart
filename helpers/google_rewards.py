3e18d46b5b6779af39fa4257c38114365d3af832
Kovvada Naga Venkata Sai Meghana <139636360+SaiMeghana14@users.noreply.github.com>
2025-07-22 13:07:54 +0530

    Update google_rewards.py

--- helpers/google_rewards.py
+++ helpers/google_rewards.py
@@ -2,30 +2,37 @@
 
 import gspread
 import streamlit as st
-from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.service_account import Credentials
 
 def get_gsheet_client():
-    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["gcp_service_account"], scope)
-    client = gspread.authorize(creds)
-    return client
+    scopes = [
+        "https://www.googleapis.com/auth/spreadsheets",
+        "https://www.googleapis.com/auth/drive"
+    ]
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=scopes
+    )
+    return gspread.authorize(creds)
 
 def get_sheet(sheet_name="Rewards"):
     client = get_gsheet_client()
-    sheet = client.open(st.secrets["private_gsheets_url"]).worksheet(sheet_name)
-    return sheet
+    spreadsheet = client.open(st.secrets["spreadsheet_name"])  # You store "EcoCart Rewards"
+    return spreadsheet.worksheet(sheet_name)
 
 def get_rewards(sheet):
-    records = sheet.get_all_records()
-    return records
+    return sheet.get_all_records()
 
 def update_rewards(sheet, user, new_points):
     records = sheet.get_all_records()
     usernames = [record["Name"] for record in records]
 
     if user in usernames:
-        row_index = usernames.index(user) + 2  # 1-based index + header row
-        current_points = int(sheet.cell(row_index, 2).value)
+        row_index = usernames.index(user) + 2  # header offset
+        try:
+            current_points = int(sheet.cell(row_index, 2).value)
+        except ValueError:
+            current_points = 0
         updated_points = current_points + new_points
         sheet.update_cell(row_index, 2, updated_points)
     else:
